97ca12e0895945f5cfcc01405b54614c5a6c9073
n35k4 <vasil.ve@outlook.com>
2022-05-30 15:13:30 +0300

    added validation

--- QuizoReact/quizo-client/src/components/Login.js
+++ QuizoReact/quizo-client/src/components/Login.js
@@ -1,39 +1,77 @@
 import { Button, Card, CardContent, TextField, Typography } from '@mui/material'
 import { Box, width } from '@mui/system'
-import React from 'react'
+import React, { useState } from 'react'
 import Center from './Center'
+import useForm from '../hooks/useForm'
+
+const getFreshModelObject = () => ({
+    name: '',
+    email: ''
+})
 
 export default function Login() {
+
+    // Add validation
+    const { values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange } = useForm(getFreshModelObject);
+
+    const login = e => {
+        e.preventDefault();
+        if (validate())
+            console.log(values);
+    }
+
+    const validate = () => {
+        let temp = {}
+        temp.email = (/\S+@\S+\.\S+/).test(values.email) ? "" : "Email is not valid!"
+        temp.name = values.name != "" ? "" : "This field is required."
+        setErrors(temp)
+        return Object.values(temp).every(x => x == "")
+    }
+
     return (
         <Center>
             <Card sx={{ width: '400px' }}>
-                <CardContent sx={{textAlign:'center'}}>
+                <CardContent sx={{ textAlign: 'center' }}>
                     <Typography variant='h3' sx={{ my: 3 }}>Quizo</Typography>
-                <Box sx={{
-                    '& .MuiTextField-root': {
-                        m: 1,
-                        width: '90%'
-                    }
-                }}>
-                    <form noValidate>
-                        <TextField
-                            label="Email"
-                            name="email"
-                            variant="outlined" />
-                        <TextField
-                            label="Name"
-                            name="name"
-                            variant="outlined" />
-                        <Button
-                            type="submit"
-                            variant="contained"
-                            size="large"
-                            sx={{ width: '90%' ,
-                            height: '50px'}}>Start</Button>
-                    </form>
-                </Box>
-            </CardContent>
-        </Card>
+                    <Box sx={{
+                        '& .MuiTextField-root': {
+                            m: 1,
+                            width: '90%'
+                        }
+                    }}>
+                        <form noValidate autoComplete='off' onSubmit={login}>
+                            <TextField
+                                label="Email"
+                                name="email"
+                                value={values.email}
+                                onChange={handleInputChange}
+                                variant="outlined"
+                                {...(errors.email && { error: true, helperText: errors.email })} 
+                                />
+                            <TextField
+                                label="Name"
+                                name="name"
+                                value={values.name}
+                                onChange={handleInputChange}
+                                variant="outlined"
+                                {...(errors.name && { error: true, helperText: errors.name })} 
+                                />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                size="large"
+                                sx={{
+                                    width: '90%',
+                                    height: '50px'
+                                }}>Start</Button>
+                        </form>
+                    </Box>
+                </CardContent>
+            </Card>
         </Center >
 
 
